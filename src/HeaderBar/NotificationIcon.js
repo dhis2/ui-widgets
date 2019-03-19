e418fb715bd2b0dd6bf581ae75674301e5698e7e
Viktor Varland <viktor@vardevs.se>
2019-03-19 12:25:06 +0100

    chore: add headerbar from ui-core

---
+++ src/HeaderBar/NotificationIcon.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import cx from 'classnames'
+import styles from './styles.js'
+
+import { colors } from '@dhis2/ui-core'
+
+function NotificationIcon({ count, href, children }) {
+    return (
+        <a className="notification" href={href}>
+            {count > 0 && <span className={cx('count')}>{count}</span>}
+            {children}
+            <style jsx>{styles}</style>
+        </a>
+    )
+}
+
+NotificationIcon.defaultProps = {
+    count: 0,
+}
+
+NotificationIcon.propTypes = {
+    children: PropTypes.element,
+    href: PropTypes.string.isRequired,
+    count: PropTypes.number,
+}
+
+export default NotificationIcon
---
+++ stories/HeaderBar.stories.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { HeaderBar } from '../src/HeaderBar'
+
+const props = {
+    baseURL: 'https://play.dhis2.org/dev',
+    instanceName: 'Sierra Leone',
+    appName: 'Import / Export',
+    profile: {
+        name: 'John Doe',
+        email: 'john_doe@dhis2.org',
+    },
+    apps: [
+        {
+            name: 'Dashboard',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Data Visualizer',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Capture',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Maintenance',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Maps',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Event Reports',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Interpretations',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Messaging',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'Import/Export',
+            path: '',
+            img: '',
+        },
+        {
+            name: 'User Settings',
+            path: '',
+            img: '',
+        },
+    ],
+}
+
+function getProps() {
+    return {
+        ...props,
+        messages: {
+            count: Math.floor(Math.random() * 40),
+        },
+        interpretations: {
+            count: Math.floor(Math.random() * 20),
+        },
+    }
+}
+
+storiesOf('HeaderBar', module).add('Default', () => (
+    <HeaderBar {...getProps()} />
+))

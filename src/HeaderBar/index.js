5a72ad99acaa5a053217b69dc0675467114917bd
Viktor Varland <viktor@vardevs.se>
2019-04-10 08:22:21 +0200

    chore: use the released app-runtime

--- src/HeaderBar/index.js
+++ src/HeaderBar/index.js
@@ -8,69 +8,63 @@ import Profile from './Profile'
 
 import css from 'styled-jsx/css'
 
-import { Query } from '@dhis2/app-service-data'
+import { useDataQuery } from '@dhis2/app-runtime'
 
 import { Logo } from './Logo.js'
 import { Title } from './Title.js'
 
 import { Notifications } from './Notifications.js'
 
-export const HeaderBar = ({ appName, className }) => (
-    <Query
-        query={{
-            systemInfo: {
-                resource: 'system/info',
-            },
-            user: {
-                resource: 'me',
-            },
-            apps: {
-                resource: '../../dhis-web-commons/menu/getModules.action',
-            },
-            notifications: {
-                resource: 'me/dashboard',
-            },
-        }}
-    >
-        {({ loading, error, data }) => {
-            if (loading) return <span>...</span>
+export const HeaderBar = ({ appName, className }) => {
+    const { loading, error, data } = useDataQuery({
+        systemInfo: {
+            resource: 'system/info',
+        },
+        user: {
+            resource: 'me',
+        },
+        apps: {
+            resource: '../../dhis-web-commons/menu/getModules.action',
+        },
+        notifications: {
+            resource: 'me/dashboard',
+        },
+    })
 
-            if (error) return <span>{`ERROR: ${error.message}`}</span>
+    if (loading) return <span>...</span>
 
-            const { systemInfo, user, apps, notifications } = data
+    if (error) return <span>{`ERROR: ${error.message}`}</span>
 
-            return (
-                <header className={className}>
-                    <Logo />
+    return (
+        <header className={className}>
+            <Logo />
 
-                    <Title app={appName} instance={systemInfo.systemName} />
+            <Title app={appName} instance={data.systemInfo.systemName} />
 
-                    <Notifications
-                        interpretations={notifications.unreadInterpretations}
-                        messages={notifications.unreadMessageConversations}
-                    />
+            <Notifications
+                interpretations={data.notifications.unreadInterpretations}
+                messages={data.notifications.unreadMessageConversations}
+            />
 
-                    <Apps apps={apps.modules} />
+            <Apps apps={data.apps.modules} />
 
-                    <Profile user={user} />
+            <Profile user={data.user} />
 
-                    <style jsx>{`
-                        header {
-                            background-color: #2c6693;
-                            display: flex;
-                            flex-direction: row;
-                            align-items: center;
-                            justify-content: space-between;
-                            height: 48px;
-                            border-bottom: 1px solid rgba(32, 32, 32, 0.15);
-                            color: ${colors.white};
-                        }
-                    `}</style>
-                </header>
-            )
-        }}
-    </Query>
-)
+            <style jsx>{`
+                header {
+                    background-color: #2c6693;
+                    display: flex;
+                    flex-direction: row;
+                    align-items: center;
+                    justify-content: space-between;
+                    height: 48px;
+                    border-bottom: 1px solid rgba(32, 32, 32, 0.15);
+                    color: ${colors.white};
+                }
+            `}</style>
+        </header>
+    )
+}
 
 HeaderBar.propTypes = {
     className: PropTypes.string,
--- stories/HeaderBar.stories.js
+++ stories/HeaderBar.stories.js
@@ -1,84 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { HeaderBar } from '../src/HeaderBar'
-import { Provider } from '@dhis2/app-service-data'
-
-const props = {
-    baseURL: 'https://play.dhis2.org/dev',
-    instanceName: 'Sierra Leone',
-    appName: 'Import / Export',
-    profile: {
-        name: 'John Doe',
-        email: 'john_doe@dhis2.org',
-    },
-    apps: [
-        {
-            name: 'Dashboard',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Data Visualizer',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Capture',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Maintenance',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Maps',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Event Reports',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Interpretations',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Messaging',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'Import/Export',
-            path: '',
-            img: '',
-        },
-        {
-            name: 'User Settings',
-            path: '',
-            img: '',
-        },
-    ],
-}
-
-function getProps() {
-    return {
-        ...props,
-        messages: {
-            count: Math.floor(Math.random() * 40),
-        },
-        interpretations: {
-            count: Math.floor(Math.random() * 20),
-        },
-    }
-}
+import { DataProvider } from '@dhis2/app-runtime'
 
 storiesOf('HeaderBar', module).add('Default', () => (
-    <Provider baseUrl="https://dhis2.vardevs.se/dev" apiVersion={32}>
+    <DataProvider baseUrl="https://dhis2.vardevs.se/dev" apiVersion={32}>
         <HeaderBar appName="Example!" />
-    </Provider>
+    </DataProvider>
 ))

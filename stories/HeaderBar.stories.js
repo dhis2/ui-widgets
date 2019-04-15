d2d77f63e9a718d4196c869d59ca0807d1d9ef23
Viktor Varland <viktor@vardevs.se>
2019-04-15 12:53:19 +0200

    refactor(headerbar): use the customdataprovider

--- stories/HeaderBar.stories.js
+++ stories/HeaderBar.stories.js
@@ -1,10 +1,78 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { HeaderBar } from '../src/HeaderBar'
-import { DataProvider } from '@dhis2/app-runtime'
+import { CustomDataProvider } from '@dhis2/app-runtime'
+
+const customData = {
+    'system/info': {
+        systemName: 'Foobar',
+    },
+    me: {
+        name: 'John Doe',
+        email: 'john_doe@dhis2.org',
+    },
+    '../../dhis-web-commons/menu/getModules.action': {
+        modules: [
+            {
+                name: 'Dashboard',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Data Visualizer',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Capture',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Maintenance',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Maps',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Event Reports',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Interpretations',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Messaging',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'Import/Export',
+                path: '',
+                img: '',
+            },
+            {
+                name: 'User Settings',
+                path: '',
+                img: '',
+            },
+        ],
+    },
+    'me/dashboard': {
+        unreadInterpretations: 10,
+        unreadMessageConversations: 5,
+    },
+}
 
 storiesOf('HeaderBar', module).add('Default', () => (
-    <DataProvider baseUrl="https://dhis2.vardevs.se/dev" apiVersion={32}>
+    <CustomDataProvider data={customData}>
         <HeaderBar appName="Example!" />
-    </DataProvider>
+    </CustomDataProvider>
 ))

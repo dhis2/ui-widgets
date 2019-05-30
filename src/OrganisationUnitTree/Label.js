ce6ced149e4ed8f3a824b719655bf93f58191fb6
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-05-30 11:47:15 +0200

    fix: disable text selection of single select only org tree

--- src/OrganisationUnitTree/Label.js
+++ src/OrganisationUnitTree/Label.js
@@ -20,6 +20,13 @@ const SingleSelectionLabel = ({ checked, label, onChange, loading }) => (
                 line-height: 26px;
                 white-space: nowrap;
                 cursor: pointer;
+                -webkit-touch-callout: none; /* iOS Safari */
+                -webkit-user-select: none; /* Safari */
+                -khtml-user-select: none; /* Konqueror HTML */
+                -moz-user-select: none; /* Firefox */
+                -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                                supported by Chrome and Opera */
             }
 
             .checked {

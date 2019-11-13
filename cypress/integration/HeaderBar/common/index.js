cfba85099ffc7c296cf52b1f26053fe0ffc62a9a
Viktor Varland <varl@vlv.io>
2019-11-13 12:57:31 +0100

    ci: add logo_banner fixtures for cypress

--- cypress/integration/HeaderBar/common/index.js
+++ cypress/integration/HeaderBar/common/index.js
@@ -27,6 +27,7 @@ Before(() => {
     cy.fixture('HeaderBar/me').as('meFixture')
     cy.fixture('HeaderBar/getModules').as('modulesFixture')
     cy.fixture('HeaderBar/dashboard').as('dashboardFixture')
+    cy.fixture('HeaderBar/logo_banner').as('logoFixture')
 })
 
 Given('the HeaderBar loads without an error', () => {
@@ -58,6 +59,13 @@ Given('the HeaderBar loads without an error', () => {
             url: 'https://domain.tld/api/me/dashboard',
             response: fx,
         }).as('dashboard')
+    })
+
+    cy.get('@logoFixture').then(fx => {
+        cy.route({
+            url: 'https://domain.tld/api/staticContent/logo_banner',
+            response: fx,
+        }).as('logo_banner')
     })
 
     cy.visitStory('HeaderBarTesting', 'Default')

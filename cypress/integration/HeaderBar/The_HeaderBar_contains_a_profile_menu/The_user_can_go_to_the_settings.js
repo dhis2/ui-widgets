4a3e04b1ba1c304dd916a34cc174bc8d5de91855
Viktor Varland <varl@vlv.io>
2019-11-29 10:37:55 +0100

    test(headerbar): update functional tests

--- cypress/integration/HeaderBar/The_HeaderBar_contains_a_profile_menu/The_user_can_go_to_the_settings.js
+++ cypress/integration/HeaderBar/The_HeaderBar_contains_a_profile_menu/The_user_can_go_to_the_settings.js
@@ -2,7 +2,7 @@ import '../common/index'
 import { Then } from 'cypress-cucumber-preprocessor/steps'
 
 Then('contains a link to the settings', () => {
-    cy.get('[data-test-id="headerbar-profile-menu-link-settings"]').should(
-        'be.visible'
-    )
+    cy.get('[data-test-id="headerbar-profile-menu"] > li').should(lis => {
+        expect(lis.eq(0)).to.be.visible
+    })
 })
--- cypress/integration/HeaderBar/The_HeaderBar_contains_a_profile_menu/The_user_can_log_out.js
+++ cypress/integration/HeaderBar/The_HeaderBar_contains_a_profile_menu/The_user_can_log_out.js
@@ -2,7 +2,7 @@ import '../common/index'
 import { Then } from 'cypress-cucumber-preprocessor/steps'
 
 Then('contains a link to log out the user', () => {
-    cy.get('[data-test-id="headerbar-profile-menu-link-logout"]').should(
-        'be.visible'
-    )
+    cy.get('[data-test-id="headerbar-profile-menu"] > li').should(lis => {
+        expect(lis.eq(4)).to.be.visible
+    })
 })

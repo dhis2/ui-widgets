84d34cf7d397a2820eb4ba73668aac00985435e0
Viktor Varland <varl@vlv.io>
2020-01-30 16:43:44 +0100

    build(app-scripts): use d2-app-scripts to build the lib

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,6 +1,16 @@
 import React, { Fragment } from 'react'
+import { addDecorator } from '@storybook/react'
 
-const FrameStylesWrapper = fn => (
+import { CssReset } from '@dhis2/ui-core'
+
+addDecorator(fn => (
+    <Fragment>
+        <CssReset />
+        {fn()}
+    </Fragment>
+))
+
+addDecorator(fn => (
     <Fragment>
         {fn()}
 
@@ -18,6 +28,4 @@ const FrameStylesWrapper = fn => (
             }
         `}</style>
     </Fragment>
-)
-
-export { FrameStylesWrapper }
+))

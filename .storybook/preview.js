293a55367f7c9dfc80c2756ac6c0fed7f3fcb4d9
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-05-15 14:20:54 +0200

    chore: fix CSS reset in built storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,34 @@
 import 'typeface-roboto'
 
-import './decorators.js'
+import React, { Fragment } from 'react'
+import { addDecorator } from '@storybook/react'
+
+import { CssReset } from '@dhis2/ui-core'
+
+addDecorator(fn => (
+    <Fragment>
+        <CssReset />
+        {fn()}
+    </Fragment>
+))
+
+addDecorator(fn => (
+    <Fragment>
+        {fn()}
+
+        <style jsx>{`
+            :global(html) {
+                height: 100%;
+            }
+            :global(body) {
+                height: 100%;
+                min-height: 100%;
+            }
+            :global(#root) {
+                height: 100%;
+                padding: 16px;
+            }
+        `}</style>
+    </Fragment>
+))
+
